c5f4246f8581c636040a65303fb0a663dbd8a601
ayrh90-fZNzxW <ayrh90@icloud.com>
2023-08-30 21:35:59 -0700

    Finished html/css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './Assets/script/script.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  
+}
+
