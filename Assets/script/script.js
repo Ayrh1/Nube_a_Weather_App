1d75e270a8257953f73a4ff5a71dd9900ed46dfb
ayrh90-fZNzxW <ayrh90@icloud.com>
2023-09-03 04:08:04 -0700

    updated css/html

---
+++ Assets/script/script.js
@@ -0,0 +1,61 @@
+/*
+
+Endpoint:
+- Please, use the endpoint api.openweathermap.org for your API calls
+- Example of API call:
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+Useful links:
+- API documentation https://openweathermap.org/api
+- Details of your plan https://openweathermap.org/price
+
+5-day forecast 
+
+let FiveWeekCast = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+
+Current day cast 
+let curretnDayCast = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+*/
+
+
+let weatherKey = "67524d354333919cc2f64508479d327c";
+
+
+$(function () {
+
+   $('body').on('click', function (event) {
+     console.log(event);
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+ --- elements and classes for search history ---
+
+   
+
+ --- elements and classes for current city temp, wind and humidity ---
+
+ 
+
+ --- elements and classes for 5 day forcast section ---
+
+    
+       
+*/
